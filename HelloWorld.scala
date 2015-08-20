fe95ae40b1e7ab59d6bef514ccda8841a1612a07
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-08-21 07:56:47 +0900

    function

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -1,3 +1,14 @@
 object HelloWorld extends App {
-    println("Hello World!")
+    val helloFunction = { string:String => println(string) }
+    helloFunction("Hello Function!")
+    println("")
+
+    (1 to 10).foreach(println)
+    println("")
+
+    (1 to 10).map(_ * 2).foreach(println)
+    println("")
+
+    (1 to 10).filter(_ % 2 == 0).foreach(println)
+    println("")
 }
